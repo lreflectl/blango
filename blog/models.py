f81f3226519a2c1d3d0ead6915d63100047af76c
randomflappy <kolyenkonexsus@gmail.com>
2022-08-16 14:12:23 +0000

    Created Comment model and its generic relation

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
 
 class Tag(models.Model):
@@ -7,6 +10,22 @@ class Tag(models.Model):
 
   def __str__(self):
     return self.value
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # connect comment to multiple (any) models
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    # content_object = GenericForeignKey('content_type', 'object_id')  # default field names can be omitted
+    content_object = GenericForeignKey()  # model to comment on (Post, User, etc.)
+
+    def __str__(self):
+      return str(self.content[:5]) + '...'
 
 
 class Post(models.Model):
@@ -19,6 +38,8 @@ class Post(models.Model):
   summary = models.TextField(max_length=500)
   content = models.TextField()
   tags = models.ManyToManyField(Tag, related_name='posts')
+
+  comments = GenericRelation(Comment)  # map comments to get them from post
 
   def __str__(self):
     return self.title
