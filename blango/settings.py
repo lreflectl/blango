99472f2a5aa9f67b8b59932fcf298d99583f9ecc
randomflappy <kolyenkonexsus@gmail.com>
2022-08-23 19:10:00 +0000

    Finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -192,9 +192,13 @@ class Dev(Configuration):
         },
     }
 
-    INTERNAL_IPS = ["192.168.11.179"]
+    INTERNAL_IPS = ["192.168.10.93"]
 
     AUTH_USER_MODEL = 'blango_auth.User'
+
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_ACTIVATION_DAYS = 7
+    REGISTRATION_OPEN = True
 
 
 class Prod(Dev):
