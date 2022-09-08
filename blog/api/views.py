928b46732e95c3fee9ff49b1d55ae47053443aa1
randomflappy <kolyenkonexsus@gmail.com>
2022-09-08 16:17:42 +0000

    Finish authentication

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,8 +3,12 @@ from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
+from rest_framework.authentication import SessionAuthentication
+
 
 class PostList(generics.ListCreateAPIView):
+    # we can define which classes to use for auth
+    # authentication_classes = [SessionAuthentication]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
