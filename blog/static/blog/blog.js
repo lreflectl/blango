8879230c4be45fbd550abf264a56dd7d3b46466e
randomflappy <kolyenkonexsus@gmail.com>
2023-08-06 13:10:00 +0000

    Finish JSX

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,43 +1,82 @@
-class ClickButton extends React.Component {
-  state = {
-    wasClicked: false
-  }
+class PostRow extends React.Component {
+  render () {
+    const post = this.props.post
 
-  handleClick () {
-    if (!this.state.wasClicked) {
-      this.setState(
-        {wasClicked: true}
-      )
+    let thumbnail
+
+    if (post.hero_image.thumbnail) {
+      thumbnail = <img src={post.hero_image.thumbnail} />
     } else {
-      this.setState(
-        {wasClicked: false}
-      )
+      thumbnail = '-'
+    }
+
+    return <tr>
+      <td>{post.title}</td>
+      <td>{thumbnail}</td>
+      <td>{post.tags.join(', ')}</td>
+      <td>{post.slug}</td>
+      <td>{post.summary}</td>
+      <td><a href={'/post/' + post.slug + '/'}>View</a></td>
+    </tr>
+  }
+}
+
+class PostTable extends React.Component {
+  state = {
+    dataLoaded: true,
+    data: {
+      results: [
+        {
+          id: 15,
+          tags: ['django', 'react'],
+          hero_image: {
+            'thumbnail': '/media/__sized__/hero_images/akatsuki_logo-thumbnail-100x100-70.jpg',
+            'full_size': '/media/hero_images/snake-419043_1920.jpg'
+          },
+          title: 'Test Post',
+          slug: 'test-post',
+          summary: 'A test post, created for Django/React.'
+        }
+      ]
     }
   }
 
   render () {
-    let buttonText
-    if (this.state.wasClicked) {
-      buttonText = 'Clicked!'
+    let rows
+    if (this.state.dataLoaded) {
+      if (this.state.data.results.length) {
+        rows = this.state.data.results.map(post => <PostRow post={post} key={post.id} />)
+      } else {
+        rows = <tr>
+          <td colSpan="6">No results found</td>
+        </tr>
+      }
     } else {
-      buttonText = 'Click Me'
+      rows = <tr>
+        <td colSpan="6">Loading&hellip;</td>
+      </tr>
     }
 
-    return React.createElement(
-      'button',
-      {
-        className: 'btn btn-primary mt-2',
-        onClick: () => {
-          this.handleClick()
-        }
-      },
-      buttonText
-    )
+    return <table className="table table-stripped table-bordered mt-2">
+      <thead>
+      <tr>
+        <th>Title</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Slug</th>
+        <th>Summary</th>
+        <th>Link</th>
+      </tr>
+      </thead>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
   }
 }
 
 const domContainer = document.getElementById('react_root')
 ReactDOM.render(
-  React.createElement(ClickButton),
+  React.createElement(PostTable),
   domContainer
 )
